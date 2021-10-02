c02b14c7770b0461a813a0adc12312d2415a3832
Anton <vsobolev45@gmail.com>
2021-10-02 17:28:59 +0300

    рисует голову собаки

--- laba4-pictures/14_1.py
+++ laba4-pictures/14_1.py
@@ -23,14 +23,43 @@ def draw_fence(surface: pygame.Surface, color: pygame.Color, rect: pygame.Rect,
         pygame.draw.line(surface, 'black', (x + dx, rect[1]), (x + dx, rect[1] + rect[3]))
         x += step
 
+# dog functions
+def draw_dog_head(surface: pygame.Surface, color: pygame.Color, middle: tuple[int, int], side: int):
+    pygame.draw.rect(surface, color, (middle[0] - side//2, middle[1] - side//2, side, side))
+
+    pygame.draw.circle(surface, color, (middle[0] - side//2, middle[1] - side//3), side//8)
+    pygame.draw.circle(surface, 'black', (middle[0] - side//2, middle[1] - side//3), side//8, width=1)
+
+    pygame.draw.circle(surface, color, (middle[0] + side//2, middle[1] - side//3), side//8)
+    pygame.draw.circle(surface, 'black', (middle[0] + side//2, middle[1] - side//3), side//8, width=1)
+def draw_dog_body():
+    return
+def draw_dog_forward_leg():
+    return
+def draw_dog_back_leg():
+    return
+def draw_dog(surface: pygame.Surface, color: pygame.Color, left_top: tuple[int, int],
+             width: int, facing_left=True, ):
+    """
+    рисует собаку как в примере
+    :param left_top: координаты левого верхнего угла изображения (x, y)
+    :param width: ширина изображения (высота вычисляется соответственно)
+    :param surface: окно, в котором рисовать
+    :param color: цвет забора
+    :param facing_left: True - собака смотрит налево, False - направо. По умолчанию True
+    :return: None
+    """
+    height = int(2/3 * width)
+    pygame.draw.rect(screen, 'grey', (left_top[0], left_top[1], width, height))
+    draw_dog_head(surface, color, (left_top[0] + width//5, left_top[1] + width//7), 2*width//7)
 
 FPS = 30
 screen = pygame.display.set_mode((600, 600))
 
 pygame.draw.rect(screen, 'lightblue', (0, 0, 600, 300))  # небо
 pygame.draw.rect(screen, 'lightgreen', (0, 300, 600, 300))  # трава
-draw_fence(screen, 'yellow', (0, 100, 600, 200), 10, 20, 5)
-
+draw_fence(screen, 'yellow', (50, 100, 400, 200), 10, 20, 5)
+draw_dog(screen, 'blue', (100, 400), 200)
 
 pygame.display.update()
 clock = pygame.time.Clock()
